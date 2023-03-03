d9858d5bcb469323f2e1f42f79cb4e345928dd54
Kshitij Verma <kshitijv250@gmail.com>
2023-03-03 22:09:17 +0530

    connected database

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const { Appointment } = require('./models');
 
 
 const app = express();
 
-
-// eslint-disable-next-line no-undef
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -16,5 +21,22 @@ app.get('/', (req, res) => {
 app.get('/appointment', (req, res) => {
     res.render('appointment');
   });
+
+app.post('/appointment', async (req, res) => {
+    try{
+        await Appointment.create({
+            name: req.body.name,
+            email: req.body.email,
+            mobile: req.body.mobile,
+            date: req.body.date,
+            from: req.body.from,
+            to: req.body.to,
+        });
+        res.redirect('/appointment');
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 
 module.exports = app;
