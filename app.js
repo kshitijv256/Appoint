a76b14b17b5e43147999e65d66b644f4658e61e3
Kshitij Verma <kshitijv250@gmail.com>
2023-03-04 11:50:36 +0530

    added more features and improved UI

--- app.js
+++ app.js
@@ -14,17 +14,19 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 async function checker(date, from, to) {
-  if (from >= to) {
-    return false;
-  }
   const appointments = await Appointment.findAll({
     where: {
       date: date,
     },
   });
   for (let i = 0; i < appointments.length; i++) {
-    if (appointments[i].from <= from && appointments[i].to >= to) {
-      console.log("Appointment already exists");
+    if (
+      (appointments[i].from < from && appointments[i].to > to) ||
+      (appointments[i].from > from && appointments[i].to < to) ||
+      (appointments[i].from < from && appointments[i].to > from) ||
+      (appointments[i].from < to && appointments[i].to > to)
+    ) {
+      console.log("Appointment time overlapping");
       return false;
     }
   }
@@ -53,6 +55,15 @@ app.get("/edit/:id", async (req, res) => {
 });
 
 app.post("/appointment", async (req, res) => {
+  if (
+    req.body.date === "" ||
+    req.body.from === "" ||
+    req.body.to === "" ||
+    req.body.from >= req.body.to
+  ) {
+    console.log("Invalid input");
+    res.redirect("/");
+  }
   const date = req.body.date;
   const from = req.body.from;
   const to = req.body.to;
@@ -66,12 +77,41 @@ app.post("/appointment", async (req, res) => {
         from: req.body.from,
         to: req.body.to,
       });
+      console.log("Appointment created");
       res.redirect("/");
     } catch (err) {
       console.log(err);
     }
   } else {
-    res.render("new", { message: "Timming is Invalid" });
+    const appointments = await Appointment.findAll({
+      where: {
+        date: date,
+      },
+      order: [["date", "ASC"]],
+    });
+    for (let i = 0; i < appointments.length; i++) {
+      if (from > appointments[i].from && from < appointments[i].to) {
+        var till = appointments[i].to;
+        var li = till.split(":");
+        if (li[0] !== "23") {
+          li[0] = parseInt(li[0]) + 1;
+          li[0] = li[0] < 10 ? "0" + li[0] : li[0];
+        } else {
+          li[0] = "00";
+          li[1] = "00";
+          li[2] = "00";
+        }
+        till = li.join(":");
+        var temp =
+          i < appointments.length - 1 ? appointments[i + 1].from : till;
+        freeSlot = {
+          from: appointments[i].to,
+          to: temp,
+        };
+      }
+    }
+    console.log(till);
+    res.redirect("/");
   }
 });
 
